f0f43d455cbbd83f90c432113ec157fef8b5ddea
JianPierDev <jianpierdev@gmail.com>
2026-08-02 13:18:50 -0500

    feat: implement Swagger for API documentation

--- questhub_api/urls.py
+++ questhub_api/urls.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView
+)
 
 urlpatterns = [
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path('admin/', admin.site.urls),
     path('api/v1/', include('users.urls'))
 ]
